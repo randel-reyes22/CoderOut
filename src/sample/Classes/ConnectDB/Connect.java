77fedcb3e2b8fec18952e21c0b23904d55c89fb1
Randel <randel.reyes@unc.edu.ph>
2020-04-21 15:28:30 +0800

    relative db and driver

--- src/sample/Classes/ConnectDB/Connect.java
+++ src/sample/Classes/ConnectDB/Connect.java
@@ -1,4 +1,33 @@
 package sample.Classes.ConnectDB;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Connect {
+    public static void Link(){
+        Connection con = null;
+        try
+        {
+            String url = "jdbc:sqlite:E:\\SQLITEDB\\Loan.db";
+
+            con = DriverManager.getConnection(url);
+            System.out.println("Connection to SQLite has been established.");
+        }
+
+        catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        finally
+        {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sample.Classes.ConnectDB.Connect;
 
 public class Main extends Application {
 
@@ -18,6 +19,7 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+        Connect.Link();
         launch(args);
     }
 }
