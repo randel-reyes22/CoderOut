5b929f9f8e0027d3a00d5134f18f6d16eb677ca0
Randel <randel.reyes@unc.edu.ph>
2020-04-24 22:59:16 +0800

    Added the necessary fxml files and controllers

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,9 +11,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../Login.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 300, 300));
         primaryStage.show();
     }
 
